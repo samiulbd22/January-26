e135f1896612adc0d53782e052b7e16ea7ad38b7
samiulbd22 <97846282+samiulbd22@users.noreply.github.com>
2026-01-11 17:57:11 +0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -94,9 +94,9 @@ const members = {
         // running month
         stay_cost       :2000,
         garage_cost     :0,
-        running_payable :0,
+        running_payable :3210,
         running_paid    :0,
-        running_meal    :5,
+        running_meal    :11,
         // method 
         preMonth,
         runningDiningCost,
@@ -114,9 +114,9 @@ const members = {
         // running month{"04/08/25-Cash":4550}
         stay_cost       :2300,
         garage_cost     :0,
-        running_payable :0,
-        running_paid    :0,
-        running_meal    :5,
+        running_payable :2667,
+        running_paid    :3500,
+        running_meal    :15,
         // method calling
         preMonth,
         runningDiningCost,
@@ -134,9 +134,9 @@ const members = {
         // running month
         stay_cost       :3250,
         garage_cost     :0,
-        running_payable :0,
-        running_paid    :0,
-        running_meal    :8,
+        running_payable :4500,
+        running_paid    :7000,
+        running_meal    :20,
         // method calling
         preMonth,
         runningDiningCost,
@@ -154,9 +154,9 @@ const members = {
         // running month
         stay_cost       :2800,
         garage_cost     :0,
-        running_payable :0,
+        running_payable :3954,
         running_paid    :0,
-        running_meal    :6,
+        running_meal    :8,
         // method calling
         preMonth,
         runningDiningCost,
@@ -170,13 +170,13 @@ const members = {
         post_payable        :3828,
         previous_paid       :6200,
         previous_dining_cost:2695,
-        payment             :undefined,
+        payment             :{"06/01/26-Cash":2500},
         // running month
         stay_cost       :3000,
         garage_cost     :0,
-        running_payable :0,
-        running_paid    :0,
-        running_meal    :10,
+        running_payable :3983,
+        running_paid    :2500,
+        running_meal    :20,
         // method calling
         preMonth,
         runningDiningCost,
@@ -191,13 +191,13 @@ const members = {
         post_payable        :3629,
         previous_paid       :5000,
         previous_dining_cost:1450,
-        payment             :undefined,
+        payment             :{"08/01/26-Cash":4000},
         // running month
         stay_cost       :2800,
         garage_cost     :0,
-        running_payable :0,
-        running_paid    :0,
-        running_meal    :3,
+        running_payable :3559,
+        running_paid    :4000,
+        running_meal    :10,
         // method calling
         preMonth,
         runningDiningCost,
@@ -211,13 +211,13 @@ const members = {
         post_payable        :4400,
         previous_paid       :5900,
         previous_dining_cost:1450,
-        payment             :undefined,
+        payment             :{"11/01/26-Cash":3500},
         // running month
         stay_cost       :3500,
         garage_cost     :0,
-        running_payable :0,
-        running_paid    :0,
-        running_meal    :0,
+        running_payable :4110,
+        running_paid    :3500,
+        running_meal    :1,
         // method calling
         preMonth,
         runningDiningCost,
@@ -231,13 +231,13 @@ const members = {
         post_payable        :4241,
         previous_paid       :6500,
         previous_dining_cost:1508,
-        payment             :undefined,
+        payment             :{"11/01/26-Cash":4000},
         // running month {"01/07/24-Cash":500}
         stay_cost       :3500,
         garage_cost     :0,
-        running_payable :0,
-        running_paid    :0,
-        running_meal    :0,
+        running_payable :3409,
+        running_paid    :4000,
+        running_meal    :1,
         // method calling
         preMonth,
         runningDiningCost,
@@ -267,19 +267,19 @@ const members = {
     },
 	"01821245430":{
         name:"Md Rubel Hossain",
-        //join:"new",
+        join:"new",
         
         // pre-previous month list
         post_payable        :1865,
         previous_paid       :2000,
         previous_dining_cost:2400,
-        payment             :undefined,
+        payment             :{"08/01/26-Cash":3000},
         // running month
         stay_cost       :0,
         garage_cost     :0,
-        running_payable :0,
-        running_paid    :0,
-        running_meal    :9,
+        running_payable :2265,
+        running_paid    :3000,
+        running_meal    :21,
         // method calling
         preMonth,
         runningDiningCost,
@@ -847,6 +847,7 @@ function sumTotal(a){
     }
     return sum;
 }
+
 
 
 
